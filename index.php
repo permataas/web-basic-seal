a41e4933d67d1b305d9d9be9e4519d82700b24e9
permataas <permataainis04@gmail.com>
2022-11-30 10:32:32 +0700

    php dasar

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World!<br>"
+
+$name = "permata";
+$umur = 18;
+
+echo "nama saya <strong>$name</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Juna";
+$umurAdik = 4;
+
+echo "nama adik <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihumur = $umur - $umurAdik
+echo "selisih usia saya dengan adik saya adalah $selisihumur tahun.";
+
+?>
